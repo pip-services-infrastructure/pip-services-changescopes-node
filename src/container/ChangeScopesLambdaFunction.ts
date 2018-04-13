660ccfa856a6be83a5c8fd19aeacf5821a327e78
Dmitriy Krayniy <krdima92@gmail.com>
2018-04-13 18:50:15 +0300

    added elasticsearch,prometheus,heartbeat

--- src/container/ChangeScopesLambdaFunction.ts
+++ src/container/ChangeScopesLambdaFunction.ts
@@ -1,5 +1,8 @@
 import { Descriptor } from 'pip-services-commons-node';
 import { CommandableLambdaFunction } from 'pip-services-aws-node';
+import { DefaultNetFactory } from 'pip-services-net-node';
+import { DefaultOssFactory } from 'pip-services-oss-node';
+
 import { ChangeScopesServiceFactory } from '../build/ChangeScopesServiceFactory';
 
 export class ChangeScopesLambdaFunction extends CommandableLambdaFunction {
@@ -7,6 +10,8 @@ export class ChangeScopesLambdaFunction extends CommandableLambdaFunction {
         super("change_scopes", "Change scopes function");
         this._dependencyResolver.put('controller', new Descriptor('pip-services-changescopes', 'controller', 'default', '*', '*'));
         this._factories.add(new ChangeScopesServiceFactory());
+        this._factories.add(new DefaultNetFactory);
+        this._factories.add(new DefaultOssFactory);
     }
 }
 
--- src/container/ChangeScopesProcess.ts
+++ src/container/ChangeScopesProcess.ts
@@ -1,5 +1,6 @@
 import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
+import { DefaultNetFactory } from 'pip-services-net-node';
 import { DefaultOssFactory } from 'pip-services-oss-node';
 
 import { ChangeScopesServiceFactory } from '../build/ChangeScopesServiceFactory';
@@ -9,8 +10,7 @@ export class ChangeScopesProcess extends ProcessContainer {
     public constructor() {
         super("change_scopes", "Change scopes microservice");
         this._factories.add(new ChangeScopesServiceFactory);
+        this._factories.add(new DefaultNetFactory);
         this._factories.add(new DefaultOssFactory);
     }
-
-
 }
