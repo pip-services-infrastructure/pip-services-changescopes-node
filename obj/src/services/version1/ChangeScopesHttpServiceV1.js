740babb54d800325e6f3fdc200be9d7bc60c26eb
pipdeveloper <pipdevs@gmail.com>
2018-05-18 16:32:44 +0000

    project build by Travis CI [skip ci]

--- obj/src/services/version1/ChangeScopesHttpServiceV1.js
+++ obj/src/services/version1/ChangeScopesHttpServiceV1.js
@@ -4,7 +4,7 @@ const pip_services_commons_node_1 = require("pip-services-commons-node");
 const pip_services_net_node_1 = require("pip-services-net-node");
 class ChangeScopesHttpServiceV1 extends pip_services_net_node_1.CommandableHttpService {
     constructor() {
-        super('change_scopes');
+        super('v1/change_scopes');
         this._dependencyResolver.put('controller', new pip_services_commons_node_1.Descriptor('pip-services-changescopes', 'controller', 'default', '*', '1.0'));
     }
 }
