c09ed7390a42fcdd958e2f336110f93db5da0b5f
EgorNuzhnykh <nuzhnykhev@gmail.com>
2019-05-22 14:17:32 +0300

    [persistence] fixed mongodb

--- obj/src/container/ChangeScopesSenecaPlugin.d.ts
+++ obj/src/container/ChangeScopesSenecaPlugin.d.ts
@@ -1,5 +1,5 @@
 import { SenecaPlugin } from 'pip-services-net-node';
 export declare class ChangeScopesSenecaPlugin extends SenecaPlugin {
     constructor(seneca: any, options: any);
-    private static createReferences(seneca, options);
+    private static createReferences;
 }
--- obj/src/index.js
+++ obj/src/index.js
@@ -8,5 +8,5 @@ __export(require("./persistence"));
 __export(require("./logic"));
 __export(require("./services/version1"));
 __export(require("./build"));
-//module.exports = require('./container/ChangeScopesSenecaPlugin'); 
+//module.exports = require('./container/ChangeScopesSenecaPlugin');
 //# sourceMappingURL=index.js.map
